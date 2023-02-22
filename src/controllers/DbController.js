1e2a42cb3ccb326135f2f32e022a04e964d635e5
Gabriel-Souza-Maia <122481268+Gabriel-Souza-Maia@users.noreply.github.com>
2023-02-21 23:28:02 -0400

    NDCG finalizado

--- src/controllers/DbController.js
+++ src/controllers/DbController.js
@@ -5,6 +5,7 @@ import request from 'request'
 import jsonfile from 'jsonfile'
 import { resolve, join } from 'path'
 import getClient from '../client/elasticsearch.js'
+import xml2js from 'xml2js'
 
 class DbController {
     async indexFromJsFiles(req, res) {
@@ -57,6 +58,153 @@ class DbController {
         }
     }
 
+    async ndcg(req, res) {
+        const parser = new xml2js.Parser({ attrkey: "ATTR" });
+        let xml_string = fs.readFileSync("./regis-collection/queries.xml", "utf8");
+        let queriesprocessadas = []
+        let idcg
+        let dcg
+        let ndcg
+        let finalresult = []
+        var array = fs.readFileSync("./regis-collection/qrels.txt").toString().split("\n");
+        for (var i in array) {
+            let pivot = array[i].replace(/\r/, '').split(" ")
+            let object = {
+                qId: pivot[0].slice(1),
+                tId: pivot[2],
+                score: Number(pivot[3])
+            }
+            queriesprocessadas.push(object)
+        }
+        let response
+        let consultas = []
+
+        parser.parseString(xml_string, function (error, result) {
+            if (error === null) {
+                for (var i = 0; i < result.root.top.length; i++) {
+                    consultas.push(result.root.top[i].title)
+                }
+            }
+            else {
+                console.log(error);
+                return res.json({ status: 500, error: error })
+            }
+        });
+        //Loop de consultas
+
+        for (i in consultas) {
+            var object = {
+                query: consultas[i].toString()
+            }
+            req.query = object
+
+            var { query } = req.query
+
+            try {
+                let data = await getClient().search({
+                    index: "regis-docs",
+                    query: {
+                        match_phrase: {
+                            text: query,
+                        },
+                    },
+                    size: 100
+                })
+
+                if (data.hits.hits.length == 0) {
+                    data = await getClient().search({
+                        index: "regis-docs",
+                        query: {
+                            multi_match: {
+                                query,
+                                fields: ["text", "filename"]
+                            },
+                        },
+                        size: 100
+                    })
+                }
+
+                response = { results: [] }
+
+                for await (var hit of data.hits.hits) {
+                    var result = {
+                        _id: hit._id,
+                        _score: hit._score,
+                        docid: hit._source.docid,
+                        filename: hit._source.filename,
+                    }
+
+                    response.results.push(result)
+                }
+
+            } catch (e) {
+                console.error(e)
+
+                return res.json({ status: 400, error: e })
+            }
+
+            //Calcular os Scores para IDCG
+            let listaDeScore = []
+
+            for (var j in queriesprocessadas) {
+                if (queriesprocessadas[j].qId == Number(i) + 1) {
+                    listaDeScore.push(queriesprocessadas[j])
+                }
+            }
+
+            listaDeScore.sort((a, b) => b.score - a.score);
+
+            //Calcular os scores para DCG
+
+            for (var j in response.results) {
+                response.results[j]._score = 0
+                for (var k in listaDeScore) {
+                    if (response.results[j]._id == listaDeScore[k].tId) {
+                        response.results[j]._score = listaDeScore[k].score
+                    }
+                }
+            }
+
+            let tam = Math.min(listaDeScore.length, response.results.length)
+
+            dcg = 0
+            for (var j in response.results) {
+                if (j < tam) {
+                    dcg = dcg + (response.results[j]._score / Math.log2(j + 2))
+                } else break
+            }
+
+            idcg = 0
+            for (var j in listaDeScore) {
+                if (j < tam) {
+                    idcg = idcg + (listaDeScore[j].score / Math.log2(j + 2))
+                } else break
+            }
+
+            ndcg = dcg / idcg
+
+            let consulta = {
+                consulta: Number(i) + 1,
+                dcg: dcg,
+                idcg: idcg,
+                ndcg: ndcg
+            }
+            finalresult.push(consulta)
+        }
+
+        let totaldcg = 0, totalidcg = 0, totalndcg = 0
+        for (var i in finalresult) {
+            totaldcg = totaldcg + finalresult[i].dcg
+            totalidcg = totalidcg + finalresult[i].idcg
+        }
+        totaldcg = totaldcg / finalresult.length
+        totalidcg = totalidcg / finalresult.length
+        totalndcg = totaldcg / totalidcg
+
+        return res.json({ status: 200, "DCG Total": totaldcg, "IDCG Total": totalidcg, "NDCG Total": totalndcg, Resultados: finalresult })
+    }
+
+
     async search(req, res) {
         const { query } = req.query
         let dcg = 0
@@ -69,7 +217,7 @@ class DbController {
                         text: query,
                     },
                 },
-                size: 10000
+                size: 50
             })
 
             if (data.hits.hits.length == 0) {
@@ -81,7 +229,7 @@ class DbController {
                             fields: ["text", "filename"]
                         },
                     },
-                    size: 10000
+                    size: 50
                 })
             }
 
--- src/server.js
+++ src/server.js
@@ -11,13 +11,15 @@ const port = process.env.PORT || 7890
 
 app.use(cors())
 
-app.get('/', (req, res) => res.json({status: 200}))
+app.get('/', (req, res) => res.json({ status: 200 }))
 
 app.get('/index-js', DbController.indexFromJsFiles)
 
 app.get('/refresh', DbController.refreshDb)
 
 app.get('/search', DbController.search)
+
+app.get('/ndcg', DbController.ndcg)
 
 app.get('/doc/:id', DbController.searchById)
 
